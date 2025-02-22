0a95c9914170edc4a5aa37c3d6fd5356ab27f11c
arefbehboudi <behboodiaref@gmail.com>
2025-02-22 12:59:20 +0330

    Init

---
+++ src/main/java/com/aref/jdb/JdbApplication.java
@@ -0,0 +1,13 @@
+package com.aref.jdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbApplication.class, args);
+	}
+
+}
